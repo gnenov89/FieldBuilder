7c369c6ab5798c7637b0824e8949c7cadc411fdf
Georgi <georgi.nenov89@gmail.com>
2022-03-16 11:03:22 -0400

    mock api as state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import MockService from './api/MockService';
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [formValues, setFormValues] = useState({})
+
+  useEffect(() => {
+    // Call api.
+    setFormValues(MockService.getField());
+    console.log(formValues)
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/api/MockService.jsx
@@ -0,0 +1,25 @@
+const FieldService = {
+  getField: function (id) {
+    return {
+      label: "Sales region",
+      required: false,
+      choices: [
+        "Asia",
+        "Australia",
+        "Western Europe",
+        "North America",
+        "Eastern Europe",
+        "Latin America",
+        "Middle East and Africa",
+      ],
+      displayAlpha: true,
+      default: "North America",
+    };
+  },
+  saveField: function (fieldJson) {
+    // Add the code here to call the API (or temporarily, just log fieldJson to the console)
+    console.log(fieldJson);
+  },
+};
+
+export default FieldService;
