0dafcbaf11f0028a06dbda796cf61450f8a646da
Georgi <georgi.nenov89@gmail.com>
2022-03-16 22:48:23 -0400

    textarea added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./index.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import MockService from "./api/MockService";
-import { Grid } from '@mui/material'
 
 function App() {
-
-
   const [formValues, setFormValues] = useState({
-    label: ""
 
   });
+
+  const textAreaElement = useRef(null);
 
   useEffect(() => {
     // Call api.
@@ -28,88 +26,95 @@ function App() {
       }
     }
 
-    if (e.target.name === "choices" && formValues.choices.includes(e.target.value)) {
-      // setFormValues({ ...formValues, choices: e.target.value })
-      console.log(e.target.value)
-
+    if (e.target.name === "choices") {
+      console.log(e.target.value);
+      textAreaElement.current.value = e.target.value;
     }
 
     if (e.target.name === "multiSelect") {
-      setFormValues({ ...formValues, required: e.target.checked })
-      console.log(e.target.value)
+      setFormValues({ ...formValues, required: e.target.checked });
+      console.log(e.target.value);
     }
 
     if (e.target.name === "alphabetical") {
-      setFormValues({ ...formValues, displayAlpha: e.target.checked })
-      console.log(e.target.value)
+      setFormValues({ ...formValues, displayAlpha: e.target.checked });
+      console.log(e.target.value);
     }
   };
 
   const submitFormToApi = (form) => {
     form.preventDefault();
-    MockService.saveField(formValues);
+    const choices = textAreaElement.current.value.split("\n");
+
+    MockService.saveField({ ...formValues, choices });
   };
 
-
-
-
   return (
-
-    <>
+    <div>
       <form id="fieldBuilderForm" onSubmit={submitFormToApi}>
+        <div className="form-row">
+          <label htmlFor="salesRegion">Label</label>
 
-        <label htmlFor="salesRegion">Label</label>
-        <input
-          type="text"
-          id="salesRegion"
-          name="label"
-          value={formValues.label !== undefined && formValues.label}
-          onChange={handleFormChange}
-        ></input>
+          <input
+            type="text"
+            id="salesRegion"
+            name="label"
+            // value={formValues.label !== undefined && formValues.label}
+            onChange={handleFormChange}
 
-        <label htmlFor="MultiSelect">MultiSelect</label>
-        <input
-          type="checkbox"
-          id="MultiSelect"
-          name="multiSelect"
-          value={formValues.required}
-          onChange={handleFormChange}
-        ></input>
+          ></input>
+          <br></br>
+        </div>
 
 
-        <label htmlFor="formChoices">CHOICES</label>
-        <select name="choices" id="formChoices" onChange={handleFormChange}>
-          {formValues.choices !== undefined &&
-            formValues.choices.map((choice, key) => {
-              return formValues.default === choice ? (
-                <option key={key} value={choice}>
-                  {choice}
-                </option>
-              ) : (
-                <option key={key} value={choice} >
-                  {choice}
-                </option>
-              );
-            })}
-        </select>
+        <div className="form-row">
+          <label htmlFor="MultiSelect">MultiSelect</label>
+          <input
+            type="checkbox"
+            id="MultiSelect"
+            name="multiSelect"
+            value={formValues.required}
+            onChange={handleFormChange}
+          ></input>
+          <br></br>
+        </div>
 
+        <div className="form-row">
+          <label htmlFor="formChoices">CHOICES</label>
+          <textarea
+            id="formChoices"
+            ref={textAreaElement}
+            rows={10}
+            name="choices"
+            defaultValue={
+              formValues.choices !== undefined
+                ? formValues.choices.join("\n")
+                : ""
+            }
+            onChange={handleFormChange}
+          ></textarea>
+          <br></br>
+        </div>
 
+        <div className="form-row">
+          <label htmlFor="alphabetical">alphabetical</label>
+          <input
+            type="checkbox"
+            id="Alphabetical"
+            name="alphabetical"
+            value={formValues.displayAlpha}
+            onChange={handleFormChange}
+          ></input>
+          <br></br>
+        </div>
 
-        <label htmlFor="alphabetical">alphabetical</label>
-        <input
-          type="checkbox"
-          id="Alphabetical"
-          name="alphabetical"
-          value={formValues.displayAlpha}
-          onChange={handleFormChange}
-        ></input>
+        <div className="form-row">
+          <button type="submit" onClick={submitFormToApi}>SUBMIT</button>
 
-        <button type="submit">SUBMIT</button>
-
-        <button type="reset">Reset</button>
+          <button type="reset">Reset</button>
+        </div>
       </form>
-
-    </>
+    </div>
   );
 }
 
