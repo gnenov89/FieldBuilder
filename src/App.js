f89e78e88cb4975c2f12612497640cb530e8a0d0
Georgi <georgi.nenov89@gmail.com>
2022-03-18 10:39:44 -0400

    almost there

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import "./index.css";
 import { useState, useEffect, useRef } from "react";
 import MockService from "./api/MockService";
@@ -5,38 +6,31 @@ import Button from "./components/Shared.jsx/Button";
 
 
 function App() {
-  //Set global state
-  const [formValues, setFormValues] = useState({ label: "" });
-  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [formValues, setFormValues] = useState({
+    label: "",
+    default: ""
+  });
   const [message, setMessage] = useState('')
-
-
-
-  const textAreaElement = useRef(null);
-  const labelInput = useRef(null)
-  const inputCheckbox = useRef(null)
-  const alphaInput = useRef(null)
+  const [btnDisabled, setBtnDisabled] = useState(true)
   useEffect(() => {
     // Call api.
     setFormValues(MockService.getField());
   }, []);
 
-
-
-
   const handleFormChange = (e) => {
     e.preventDefault();
+
     if (e.target.name === "label") {
-      // console.log(e.target.value)
+      console.log(e.target.value);
       if (e.target.value === "") {
         setFormValues({ ...formValues, label: "" });
-        setBtnDisabled(true)
         setMessage(null)
-      }
-      else if (e.target.value !== "" && e.target.value.trim().length <= 10) {
+      } else if (e.target.value !== "" && e.target.value.trim().length <= 10) {
+        setFormValues({ ...formValues, label: e.target.value })
         setBtnDisabled(true)
         setMessage('Text must be at least 10 characters')
-        setFormValues({ ...formValues, label: e.target.value })
+
+        // console.log(e.target.value)
       }
       else {
         setFormValues({ ...formValues, label: e.target.value });
@@ -44,151 +38,137 @@ function App() {
         setBtnDisabled(false)
 
       }
-
     }
-
-
 
     if (e.target.name === "choices") {
-      // let stringValues = e.target.value
-      // let defaultValue = formValues.default
-      // const myArray = stringValues.split("\n")
-      // if (!stringValues.includes(formValues.default)) {
-      //   stringValues.push(formValues.default)
-      // }
-
+      console.log(e.target.value);
       textAreaElement.current.value = e.target.value;
-
-      // if (!myArray.includes(formValues.default)) {
-      //   myArray.push(formValues.default)
-      //  
-
-      // }
-
-
-
     }
 
-    if (e.target.name === "multiSelect" && e.target.checked) {
-      setFormValues({ ...formValues, required: true });
-      // console.log(formValues);
-    } else {
-
-      setFormValues({ ...formValues, required: false });
-      // console.log(formValues);
+    if (e.target.name === "multiSelect") {
+      setFormValues({ ...formValues, required: e.target.checked });
+      // console.log(e.target.value);
     }
-
 
     if (e.target.name === "alphabetical") {
       setFormValues({ ...formValues, displayAlpha: e.target.checked });
-      console.log(e.target.value);
+      // console.log(e.target.value);
     }
-
   };
 
-  //Clear Input field 
+  const labelInput = useRef(null)
+  const defValue = useRef(null)
+  const textAreaElement = useRef(null);
+  const inputCheckbox = useRef(null)
+  const alphaInput = useRef(null)
+
   const clearInputFields = (form) => {
+    // setFormValues({})
     form.preventDefault()
-    setFormValues({
-    })
     textAreaElement.current.value = ""
     labelInput.current.value = ""
+    defValue.current.value = ""
     inputCheckbox.current.checked = false
     alphaInput.current.checked = false
     console.log(formValues)
   }
-  // Submit to API 
   const submitFormToApi = (form) => {
     form.preventDefault();
-    const choices = textAreaElement.current.value.split("\n");
+    let choices = textAreaElement.current.value.split("\n");
+    let defAult = defValue.current.value
+    if (!choices.includes(defAult)) {
+      choices.push(defAult)
+      let defAdd = textAreaElement.current.value.concat(",", defValue.current.value)
+      setFormValues({ ...formValues, choices: defAdd });
+    }
+
     MockService.saveField({ ...formValues, choices });
-    console.log(formValues)
-  }
+
+    // let myArr = textAreaElement.current.value.split("\n")
+    // if (!myArr.includes(defAult)) {
+    //   myArr.push(defAult)
+    // }
+    // console.log(myArr)
+    // MockService.saveField({ ...formValues, myArr });
+
+  };
 
   return (
+    <>
+      <form id="fieldBuilderForm" onSubmit={submitFormToApi}>
+        <label htmlFor="salesRegion">Label</label>
+        <input
+          type="text"
+          id="salesRegion"
+          name="label"
+          ref={labelInput}
+          // value={formValues.label !== undefined && formValues.label}
+          onChange={handleFormChange}
+        ></input>
+        <br></br>
 
+        {message && <div className="message">{message}</div>}
 
-    <form id="fieldBuilderForm" className="container" onSubmit={submitFormToApi}>
+        <label htmlFor="Default Value">Default </label>
+        <input
+          ref={defValue}
+          type="text"
+          id="salesRegion"
+          name="default"
+          value={formValues.default}
+          onChange={handleFormChange}
+        ></input>
+        <br></br>
 
-      <header>FieldBuilder</header>
+        <label htmlFor="MultiSelect">MultiSelect</label>
+        <input
+          ref={inputCheckbox}
+          type="checkbox"
+          id="MultiSelect"
+          name="multiSelect"
+          value={formValues.required}
+          onChange={handleFormChange}
+        ></input>
+        <br></br>
 
-
-
-      <label htmlFor="salesRegion">Label</label>
-      <input
-        ref={labelInput}
-        type="text"
-        id="salesRegion"
-        name="label"
-        // value={formValues.label !== undefined && formValues.label}
-        placeholder="Sales Region"
-        onChange={handleFormChange}
-      ></input>
-      <br></br>
-
-
-      {message && <div className="message">{message}</div>}
-
-
-      <label htmlFor="MultiSelect">MultiSelect</label>
-      <input
-        ref={inputCheckbox}
-        type="checkbox"
-        id="MultiSelect"
-        name="multiSelect"
-        value={formValues.required}
-        onChange={handleFormChange}
-      ></input>
-      <br></br>
-
-
-
-      <label htmlFor="formChoices">CHOICES</label>
-      <textarea
-        id="formChoices"
-        ref={textAreaElement}
-        rows={10}
-        name="choices"
-        defaultValue={
-          formValues.choices !== undefined
-            ? formValues.choices.join("\n")
-            : ""
-        }
-        onChange={handleFormChange}
-      ></textarea>
-      <br></br>
-
-
-
-      <label htmlFor="alphabetical">alphabetical</label>
-      <input
-        ref={alphaInput}
-        type="checkbox"
-        id="Alphabetical"
-        name="alphabetical"
-        value={formValues.displayAlpha}
-        onChange={handleFormChange}
-      ></input>
-      <br></br>
-
-
-      {/* <button name="reset" type="reset" className="btn-primary" onClick={clearInputFields}>Reset</button> */}
-
-      {/* <button type="submit" onClick={submitFormToApi}>SUBMIT</button> */}
-
-      <Button type="submit" onClick={submitFormToApi} isDisabled={btnDisabled}>SUBMIT</Button>
-
-      <Button type="reset" onClick={clearInputFields} version="secondary">CLEAR</Button>
+        <label htmlFor="formChoices">CHOICES</label>
+        <textarea
+          id="formChoices"
+          ref={textAreaElement}
+          rows={10}
+          name="choices"
+          defaultValue={
+            formValues.choices !== undefined
+              ? formValues.choices
+              : ""
+          }
+          onChange={handleFormChange}
+        ></textarea>
+        <br></br>
 
 
 
-    </form >
+        <label htmlFor="alphabetical">alphabetical</label>
+        <input
+          ref={alphaInput}
+          type="checkbox"
+          id="Alphabetical"
+          name="alphabetical"
+          value={formValues.displayAlpha}
+          onChange={handleFormChange}
+        ></input>
+        <br></br>
 
+
+        <Button type="submit" onClick={submitFormToApi} isDisabled={btnDisabled}>SUBMIT</Button>
+
+        <Button type="reset" onClick={clearInputFields} version="secondary">CLEAR</Button>
+      </form>
+    </>
   );
 }
 
 export default App;
-
 
 
 
